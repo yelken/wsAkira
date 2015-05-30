0689adf28a0742e845ae1b1dcb1be46ba184aede
victorkaor <victonboy@hotmail.com>
2015-05-30 15:16:32 -0300

    Criação da Classe Ponto

---
+++ src/br/com/akira/ponto/Ponto.java
@@ -0,0 +1,26 @@
+package br.com.akira.ponto;
+
+import br.com.akira.basica.Basica;
+
+public class Ponto extends Basica {
+
+	private Double latitude;
+	private Double longitude;
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+}
