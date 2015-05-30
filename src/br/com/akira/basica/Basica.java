6b80acd5fa4d7a647309448b28bf1575a46cc133
Yelken Gonzales <yelkezin@gmail.com>
2015-05-30 15:09:43 -0300

    Commit da classe básica

---
+++ src/br/com/akira/basica/Basica.java
@@ -0,0 +1,17 @@
+package br.com.akira.basica;
+
+public abstract class Basica {
+	
+	private Integer identifier;
+
+	public Integer getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(Integer identifier) {
+		this.identifier = identifier;
+	}
+	
+	
+
+}
